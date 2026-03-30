40b5464ada6d4fc689d63bbd1f91c77c249b4418
xuzifu666 <1206332514@qq.com>
2026-03-30 16:19:33 +0800

    fix styles

--- extension/core_functions/scalar/string/crc32.cpp
+++ extension/core_functions/scalar/string/crc32.cpp
@@ -38,11 +38,11 @@ static const uint32_t my_crc32_table[256] = {
     0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d};
 
 static uint32_t my_crc32(const char *data, size_t length) {
-	   uint32_t crc = 0xFFFFFFFF;
-	   for (size_t i = 0; i < length; ++i) {
-	   	       crc = my_crc32_table[(crc ^ data[i]) & 0xFF] ^ (crc >> 8);
-	   }
-	   return crc ^ 0xFFFFFFFF;
+	uint32_t crc = 0xFFFFFFFF;
+	for (size_t i = 0; i < length; ++i) {
+		crc = my_crc32_table[(crc ^ data[i]) & 0xFF] ^ (crc >> 8);
+	}
+	return crc ^ 0xFFFFFFFF;
 }
 
 struct CRC32Operator {
